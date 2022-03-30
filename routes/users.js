39f989e9201d3b2573a3cbc10b4dbe9828bfe8e3
ikhide <ikhide.atakpu@gmail.com>
2022-03-30 22:48:15 +0100

    script

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 const router = require("express").Router();
 const { SHA3, SHAKE } = require("sha3");
 const bcrypt = require("bcrypt");
-const { pbkdf2, pbkdf2Sync } = require("crypto");
+const { pbkdf2, pbkdf2Sync, scrypt } = require("crypto");
+const salt = require("../config/keys").secretOrKey;
 
 //create an array of 1 to 1000
-const array = Array.from({ length: 1000 }, (v, k) => k + 1);
+const array1000 = Array.from({ length: 1000 }, (v, k) => k + 1);
 //create an array of 1 to 1000000
-const array2 = Array.from({ length: 1000000 }, (v, k) => k + 1);
+const array1000000 = Array.from({ length: 1000000 }, (v, k) => k + 1);
 
 router.post("/sha512", async (req, res) => {
   try {
@@ -15,7 +16,7 @@ router.post("/sha512", async (req, res) => {
 
     //hash the array and return time to complete
     const start = new Date().getTime();
-    array.map((item) => hash.update(item.toString()).digest("hex"));
+    array1000.map((item) => hash.update(item.toString()).digest("hex"));
     const end = new Date().getTime();
     const time1 = end - start;
 
@@ -44,7 +45,7 @@ router.post("/sha256", async (req, res) => {
 
     //hash the array and return time to complete
     const start = new Date().getTime();
-    array.map((item) => hash.update(item.toString()).digest("hex"));
+    array1000.map((item) => hash.update(item.toString()).digest("hex"));
     const end = new Date().getTime();
     const time1 = end - start;
 
@@ -56,7 +57,6 @@ router.post("/sha256", async (req, res) => {
 
     //return the time to complete
     res.json({ "1000 numbers": time1, "1000000 numbers": time2 });
-
     return;
   } catch (err) {
     console.log({ err });
@@ -72,7 +72,7 @@ router.post("/shake128", async (req, res) => {
 
     //hash the array and return time to complete
     const start = new Date().getTime();
-    array.map((item) => hash.update(item.toString()).digest("hex"));
+    array1000.map((item) => hash.update(item.toString()).digest("hex"));
     const end = new Date().getTime();
     const time1 = end - start;
 
@@ -101,7 +101,7 @@ router.post("/shake256", async (req, res) => {
 
     //hash the array and return time to complete
     const start = new Date().getTime();
-    array.map((item) => hash.update(item.toString()).digest("hex"));
+    array1000.map((item) => hash.update(item.toString()).digest("hex"));
     const end = new Date().getTime();
     const time1 = end - start;
 
@@ -125,13 +125,13 @@ router.post("/bcrypt", async (req, res) => {
   try {
     //hash the array and return time to complete
     const start = new Date().getTime();
-    array.map((item) => bcrypt.hashSync(item.toString(), 10));
+    array1000.map((item) => bcrypt.hashSync(item.toString(), 10));
     const end = new Date().getTime();
     const time1 = end - start;
 
     //hash the array and return time to complete
     const start2 = new Date().getTime();
-    array2.map((item) => bcrypt.hashSync(item.toString(), 10));
+    // array1000000.map((item) => bcrypt.hashSync(item, 10));
     const end2 = new Date().getTime();
     const time2 = end2 - start2;
 
@@ -148,13 +148,17 @@ router.post("/bcryptAsync", async (req, res) => {
   try {
     //hash the array and return time to complete
     const start = new Date().getTime();
-    await Promise.all(array.map((item) => bcrypt.hash(item.toString(), 10)));
+    await Promise.all(
+      array1000.map((item) => bcrypt.hash(item.toString(), 10))
+    );
     const end = new Date().getTime();
     const time1 = end - start;
 
     //hash the array and return time to complete
     const start2 = new Date().getTime();
-    await Promise.all(array2.map((item) => bcrypt.hash(item.toString(), 10)));
+    // await Promise.all(
+    //   array1000000.map((item) => bcrypt.hash(item.toString(), 10))
+    // );
     const end2 = new Date().getTime();
     const time2 = end2 - start2;
 
@@ -171,16 +175,16 @@ router.post("/pbkdf2", async (req, res) => {
   try {
     //hash the array and return time to complete
     const start = new Date().getTime();
-    array.map((item) =>
-      pbkdf2Sync(item.toString(), "salt", 100000, 128, "sha512")
+    array1000.map((item) =>
+      pbkdf2Sync(item.toString(), salt, 10, 128, "sha512")
     );
     const end = new Date().getTime();
     const time1 = end - start;
 
     //hash the array and return time to complete
     const start2 = new Date().getTime();
-    array2.map((item) =>
-      pbkdf2Sync(item.toString(), "salt", 100000, 128, "sha512")
+    array1000000.map((item) =>
+      pbkdf2Sync(item.toString(), salt, 10, 128, "sha512")
     );
     const end2 = new Date().getTime();
     const time2 = end2 - start2;
@@ -199,8 +203,8 @@ router.post("/pbkdf2Async", async (req, res) => {
     //hash the array and return time to complete
     const start = new Date().getTime();
     await Promise.all(
-      array.map((item) =>
-        asyncPbkdf2(item.toString(), "salt", 100000, 128, "sha512")
+      array1000.map((item) =>
+        asyncPbkdf2(item.toString(), salt, 10, 128, "sha512")
       )
     );
     const end = new Date().getTime();
@@ -208,11 +212,11 @@ router.post("/pbkdf2Async", async (req, res) => {
 
     //hash the array and return time to complete
     const start2 = new Date().getTime();
-    // await Promise.all(
-    //   array2.map((item) =>
-    //     pbkdf2(item.toString(), "salt", 100000, 128, "sha512")
-    //   )
-    // );
+    await Promise.all(
+      array1000000.map((item) =>
+        asyncPbkdf2(item.toString(), salt, 10, 128, "sha512")
+      )
+    );
     const end2 = new Date().getTime();
     const time2 = end2 - start2;
 
@@ -228,6 +232,48 @@ router.post("/pbkdf2Async", async (req, res) => {
 const asyncPbkdf2 = (password, salt, iterations, keylen, digest) => {
   return new Promise((resolve, reject) => {
     pbkdf2(password, salt, iterations, keylen, digest, (err, derivedKey) => {
+      if (err) reject(err);
+      else resolve(derivedKey);
+    });
+  });
+};
+
+//scrypt async hash
+router.post("/scrypt", async (req, res) => {
+  try {
+    //hash the array and return time to complete
+    const start = new Date().getTime();
+    await Promise.all(
+      array1000.map((item) =>
+        scryptHash(item.toString(), salt, 128, { N: 512 })
+      )
+    );
+    const end = new Date().getTime();
+    const time1 = end - start;
+
+    //hash the array and return time to complete
+    const start2 = new Date().getTime();
+    await Promise.all(
+      array1000000.map((item) =>
+        scryptHash(item.toString(), salt, 128, { N: 512 })
+      )
+    );
+
+    const end2 = new Date().getTime();
+    const time2 = end2 - start2;
+
+    //return the time to complete
+    res.json({ "1000 numbers": time1, "100000 numbers": time2 });
+  } catch (err) {
+    console.log({ err });
+    res.status(500).json({ err });
+  }
+});
+
+//scrypt hash promise
+const scryptHash = (password, salt, keylen, options) => {
+  return new Promise((resolve, reject) => {
+    scrypt(password, salt, keylen, options, (err, derivedKey) => {
       if (err) reject(err);
       else resolve(derivedKey);
     });
